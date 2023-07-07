50c715c52474ab9d43e8c58e75dc4d9c0897af4a
lgweida <45736060+lgweida@users.noreply.github.com>
2023-07-07 03:32:48 +0000

    format json

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import requests
+import json
+
 res = requests.get("http://129.213.18.27:5010/")
-print(res.json())
+
+json_object = json.loads(res.json())
+
+json_formatted_str = json.dumps(json_object, indent=2)
+
+print(json_formatted_str)
+
+#print(res.json())
